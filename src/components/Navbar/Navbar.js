405fc140e982cf770920b534310b4802c5d19c16
Dmitry Cherkashin <dchpost@mail.ru>
2024-12-23 11:43:50 +1000

    добавляет темную тему

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,5 @@
-import sun from "./../../img/icons/sun.svg"
-import moon from "./../../img/icons/moon.svg"
-
 import { NavLink } from "react-router-dom"
-
+import BtnDarkMode from "../btnDarkMode/BtnDarkMode"
 import "./styles.css"
 
 function Navbar() {
@@ -16,11 +13,7 @@ function Navbar() {
                     <NavLink to="/" className="logo">
                         <strong>Frontender</strong> portfolio
                     </NavLink>
-
-                    <button className="dark-mode-btn">
-                        <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-                        <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-                    </button>
+                    <BtnDarkMode/>
 
                     <ul className="nav-list">
                         <li className="nav-list__item">
